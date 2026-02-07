fcdcf9bb05fee92a3425c7c899163381739f8c5a
Bekmukhamed <bekmukhamed.tursunbayev@gmail.com>
2026-02-07 14:09:19 -0800

    Fix: dynamodb key schema

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,11 +52,22 @@ resource "aws_s3_bucket_public_access_block" "raw_emails" {
 resource "aws_dynamodb_table" "aliases" {
   name           = "${var.project_name}-aliases"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "alias_email"
+  hash_key       = "alias"
+  range_key      = "status"
 
   attribute {
-    name = "alias_email"
+    name = "alias"
     type = "S"
+  }
+
+  attribute {
+    name = "status"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "expiration_time"
+    enabled        = true
   }
 
   tags = {
